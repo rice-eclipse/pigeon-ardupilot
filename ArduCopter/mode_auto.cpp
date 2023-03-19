9d2b1e1f29f5695672d69adedbf56763e6bb6c1e
irrundle3 <irrundle3@github.com>
2023-03-19 11:41:35 -0500

    removed safety feature from mode_auto requiring a takeoff command as the first mission command. Since auto will be activated mid-air, it makes no sense for us to have a takeoff command

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -25,10 +25,13 @@ bool ModeAuto::init(bool ignore_checks)
     auto_RTL = false;
     if (mission.num_commands() > 1 || ignore_checks) {
         // reject switching to auto mode if landed with motors armed but first command is not a takeoff (reduce chance of flips)
-        if (motors->armed() && copter.ap.land_complete && !mission.starts_with_takeoff_cmd()) {
-            gcs().send_text(MAV_SEVERITY_CRITICAL, "Auto: Missing Takeoff Cmd");
-            return false;
-        }
+
+        // Ian: took out this safety feature because we don't want to start our mission with takeoff
+
+        // if (motors->armed() && copter.ap.land_complete && !mission.starts_with_takeoff_cmd()) {
+        //     gcs().send_text(MAV_SEVERITY_CRITICAL, "Auto: Missing Takeoff Cmd");
+        //     return false;
+        // }
 
         _mode = SubMode::LOITER;
 
